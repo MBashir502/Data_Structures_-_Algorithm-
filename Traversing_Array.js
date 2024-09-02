03ac67b89d19542c931ceb28862cabf1f13a41e1
Muhammad Bashir <muhammadbashir0324@gmail.com>
2024-09-02 11:01:39 +0500

    Traversing Array print indexing Array is...

---
+++ Traversing_Array.js
@@ -0,0 +1,6 @@
+// Traversing an Array in Js
+let data = [10, 20, 30, 40, 50, 60, 70];
+// Traversing an Array
+for (let i = 0; i < data.length; i++) {
+  console.log(`index: ${i} is ${data[i]}`);
+}
